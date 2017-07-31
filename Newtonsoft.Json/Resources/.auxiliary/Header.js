594233f1b18e0b4b701b17a65705755c78a82e52
LeonidVeriga <leonid@object.net>
2017-07-31 12:20:43 +0300

    Update to 16.0.0

--- Newtonsoft.Json/Resources/.auxiliary/Header.js
+++ Newtonsoft.Json/Resources/.auxiliary/Header.js
@@ -1,7 +1,7 @@
 ﻿/*
  * @version   : {version} - A Bridge.NET implementation of Newtonsoft.Json
  * @author    : Object.NET, Inc. http://www.bridge.net/
- * @date      : 2017-07-25
+ * @date      : 2017-08-01
  * @copyright : Copyright (c) 2008-{year}, Object.NET, Inc. (http://www.object.net/). All rights reserved.
  * @license   : See license.txt and https://github.com/bridgedotnet/Bridge.NET/blob/master/LICENSE.
  */
