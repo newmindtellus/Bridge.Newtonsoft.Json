b3c2a8d1b00c99c14f1079ab17270e4fe4bc0fd2
LeonidVeriga <leonid@object.net>
2017-08-20 17:41:32 +0300

    Bump v1.1.0 of 2017-08-20

--- Tests/Runner/General/code.js
+++ Tests/Runner/General/code.js
@@ -1,6 +1,6 @@
 /**
  * Newtonsoft.Json Test library
- * @version 1.0.0
+ * @version 1.1.0
  * @author Object.NET, Inc.
  * @copyright Copyright 2008-2017 Object.NET, Inc.
  * @compiler Bridge.NET 16.1.0
--- Tests/Runner/General/newtonsoft.json.js
+++ Tests/Runner/General/newtonsoft.json.js
@@ -1,7 +1,7 @@
 /*
- * @version   : 1.0.0 - A Bridge.NET implementation of Newtonsoft.Json
+ * @version   : 1.1.0 - A Bridge.NET implementation of Newtonsoft.Json
  * @author    : Object.NET, Inc. http://www.bridge.net/
- * @date      : 2017-08-01
+ * @date      : 2017-08-20
  * @copyright : Copyright (c) 2008-2017, Object.NET, Inc. (http://www.object.net/). All rights reserved.
  * @license   : See license.txt and https://github.com/bridgedotnet/Bridge.NET/blob/master/LICENSE.
  */
