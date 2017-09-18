f665d62d9a2daa66646e1a965ae8d5acad876c31
LeonidVeriga <leonid@object.net>
2017-09-18 15:09:55 +0300

    Remove @date from JS output

--- Newtonsoft.Json/Resources/.auxiliary/Header.js
+++ Newtonsoft.Json/Resources/.auxiliary/Header.js
@@ -1,7 +1,6 @@
 ﻿/*
  * @version   : {version} - A Bridge.NET implementation of Newtonsoft.Json
  * @author    : Object.NET, Inc. http://www.bridge.net/
- * @date      : 2017-08-23
  * @copyright : Copyright (c) 2008-{year}, Object.NET, Inc. (http://www.object.net/). All rights reserved.
  * @license   : See license.txt and https://github.com/bridgedotnet/Bridge.NET/blob/master/LICENSE.
  */
--- Tests/Runner/General/newtonsoft.json.js
+++ Tests/Runner/General/newtonsoft.json.js
@@ -1,7 +1,6 @@
 /*
  * @version   : 1.2.0 - A Bridge.NET implementation of Newtonsoft.Json
  * @author    : Object.NET, Inc. http://www.bridge.net/
- * @date      : 2017-08-23
  * @copyright : Copyright (c) 2008-2017, Object.NET, Inc. (http://www.object.net/). All rights reserved.
  * @license   : See license.txt and https://github.com/bridgedotnet/Bridge.NET/blob/master/LICENSE.
  */
